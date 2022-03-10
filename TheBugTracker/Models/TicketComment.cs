93da56755111e494366aa9ecb0b46502bdbe8fc7
Kharn Tupmongkol <kharnthorn@gmail.com>
2022-03-10 23:01:03 +0000

    created the ticket model and its' relevent models

---
+++ TheBugTracker/Models/TicketComment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class TicketComment
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketID { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        //Navigation properties
+        //Ticket
+        public virtual Ticket Ticket { get; set; }
+        //User
+        public virtual BTUser User { get; set; }
+    }
+}
