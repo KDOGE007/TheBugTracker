f3b10bd4397528e8fb33ecbb39dc3bd80bb4bbd2
Kharn Tupmongkol <kharnthorn@gmail.com>
2022-03-13 20:00:48 +0000

    add views for all the models

--- TheBugTracker/Models/Company.cs
+++ TheBugTracker/Models/Company.cs
@@ -21,6 +21,6 @@ namespace TheBugTracker.Models
         public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
         public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
 
-        //creat relationship to invites
+        //create relationship to invites
     }
 }
